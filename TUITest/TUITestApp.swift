97f6f9d750f0ff713ff01af4c5c450adbad39b4f
Danil <dashc@ciklum.com>
2022-08-22 16:41:05 +0300

    Initial Commit

---
+++ TUITest/TUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TUITestApp.swift
+//  TUITest
+//
+//  Created by Danil on 22.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
